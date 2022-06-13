f952ef30ac199ca2f87ad8fe0db11a41944d4d37
Prasansa Bhagat <75807786+prasansabhagat@users.noreply.github.com>
2022-06-14 00:07:00 +0530

    initials

---
+++ Tkinter/basic.py
@@ -0,0 +1,12 @@
+#NOTE: the file name cannot be tkinter.py!!
+import tkinter as tk
+from tkinter import ttk
+
+#tk._test()
+
+root = tk.Tk()
+
+ttk.Label(root, text="Hello World!", padding=(30,10)).pack()  #center aligned and packed
+#ttk.Label(root, text="Hello World!").grid() ----not center aligned
+
+root.mainloop()
---
+++ Tkinter/button.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+def greet():
+    print("Hello!")
+
+root = tk.Tk()
+
+greet_button = ttk.Button(root, text="Greet", command=greet)
+greet_button.pack(side="left", fill="y", expand=True)
+
+quit_button = ttk.Button(root, text = "Quit", command=root.destroy)
+quit_button.pack()
+
+root.mainloop()
