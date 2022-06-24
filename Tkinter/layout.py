11ed5260be7761e0520b5831397939942e440be7
Prasansa Bhagat <75807786+prasansabhagat@users.noreply.github.com>
2022-06-24 17:31:41 +0530

    funtions upload

--- Tkinter/layout.py
+++ Tkinter/layout.py
@@ -63,13 +63,68 @@ def encode_text(frame):
     home_button = Button(frame, text="Home", command=lambda: home(frame))
     home_button.grid(row=5, column=0, pady=20)
 
+#function to generate text data(message to hide) to binary
+def generate_data(data):
+    new_data = []
+    for i in data:
+        new_data.append(format(ord(i), "08b"))  
+        #format(14, '08b') --> 00001110 , coverts to 8bit binary
+        #ord function returns the ascii(unicode value) value
+    print("new data")
+    print(new_data)
+    return new_data
+
+#function to modify the pixels of image
+def modify_pixel(pixel, data):
+    data_list = generate_data(data)
+    data_length = len(data_list)
+    print(data_length)
+    image_data = iter(pixel) 
+    print(image_data)
+    print("bye")
+    #iter --> function creates an object which can be iterated one element at a time.
+    for i in range(data_length):
+        pixel = [value for value in image_data.__next__()[:3] + image_data.__next__()[:3] + image_data.__next__()[:3]]
+        for j in range(0, 8):
+            if (data_list[i][j] == '0') and (pixel[j] % 2 != 0):
+                if (pixel[j] % 2 != 0):
+                    pixel[j] -= 1
+
+                elif (data_list[i][j] == '1') and (pixel[j] % 2 == 0):
+                    pixel[j] -= 1
+
+        if (i == data_length - 1):
+            if (pixel[-1] % 2 == 0):
+                pixel[-1] -= 1
+        else:
+            if (pixel[-1] % 2 != 0):
+                pixel[-1] -= 1
+
+        pixel = tuple(pixel)
+        yield pixel[0:3]
+        yield pixel[3:6]
+        yield pixel[6:9]
+
+
+
+#function to enter the data pixels in image
+def encode_data_pixels(new_img, data):
+    w = new_img.size[0]
+    (x,y) = (0,0)
+    for pixels in modify_pixel(new_img.getdata(), data):
+        print("bye")
+
 #function to enter hidden text
 def encode_function(textbox, my_img):
+    global data
     data = textbox.get("1.0", "end-1c")
     if (len(data) == 0):
         tk.messagebox.showinfo("Alert", "Kindly enter text in TextBox")
     else:
-        print("Bye")
+        global new_img
+        new_img = my_img.copy()
+        encode_data_pixels(new_img, data)
+
 
 #function to open file for encoding
 def open_encode_file(frame):
